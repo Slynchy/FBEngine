420de7467165965b80f542904265f798a7677358
Sam Lynch <sam.lynch2@hotmail.co.uk>
2018-03-13 00:49:40 +0100

    [BRe-5] Implement interstitial adverts

--- Adverts.js
+++ Adverts.js
@@ -3,7 +3,9 @@ class Adverts {
 
     constructor(){
         this.placementId = Settings.adverts.placementId;
-        this.instance = null;
+
+        this.rewarded_instance = null;
+        this.interstitial_instance = null;
 
         this._loaded = false;
 
@@ -20,7 +22,7 @@ class Adverts {
 
         console.log("[Adverts] Initializing ad instance");
 
-        if(this.enabled === false){
+        if(this.enabled === false || typeof FBInstant === 'undefined'){
             if(callback)
                 callback();
             if(waitForLoad)
@@ -28,14 +30,37 @@ class Adverts {
             return;
         }
 
-        FBInstant.getRewardedVideoAsync(this.placementId)
-            .then((rewardedVideo) => {
-                console.log("[Adverts] Loaded ad instance");
-                self.instance = rewardedVideo;
-                self.instance.loadAsync()
+        let rewardedAdDone = false;
+        let interstitialAdDone = false;
+        this._getAndLoadRewardedAdInstance(()=>{
+            rewardedAdDone = true;
+            if(interstitialAdDone === true){
+                if(callback){
+                    callback();
+                }
+            }
+        });
+
+        this._getAndLoadInterstitialAdInstance(()=>{
+            interstitialAdDone = true;
+            if(rewardedAdDone === true){
+                if(callback){
+                    callback();
+                }
+            }
+        });
+    }
+
+    _getAndLoadInterstitialAdInstance(callback, waitForLoad){
+        if(this.interstitial_instance) return;
+
+        FBInstant.getInterstitialAdAsync(this.placementId)
+            .then((interstitial) => {
+                console.log("[Adverts] Loaded ad rewarded_instance");
+                self.interstitial_instance = interstitial;
+                self.interstitial_instance.loadAsync()
                     .then( (retVal) => {
-                        console.log("[Adverts] Preloaded ad");
-                        self._loaded = true;
+                        console.log("[Adverts] Preloaded interstitial ad");
                         if(waitForLoad) waitForLoad();
                     })
                     .catch( (err) => {
@@ -47,7 +72,7 @@ class Adverts {
                                 break;
                         }
 
-                        console.error('[Adverts] Load ad failed! ' + err.code);
+                        console.error('[Adverts] Load interstitial ad failed! ' + err.code);
                         console.error(err.message);
 
                         if(waitForLoad) waitForLoad();
@@ -61,7 +86,7 @@ class Adverts {
                         break;
                 }
 
-                console.error('[Adverts] Get ad instance failed! ' + err.code);
+                console.error('[Adverts] Get ad interstitial failed! ' + err.code);
                 console.error(err.message);
 
                 if(callback) callback();
@@ -69,7 +94,50 @@ class Adverts {
             });
     }
 
-    showAd(callback){
+    _getAndLoadRewardedAdInstance(callback, waitForLoad){
+        if(this.rewarded_instance) return;
+
+        FBInstant.getRewardedVideoAsync(this.placementId)
+            .then((rewardedVideo) => {
+                console.log("[Adverts] Loaded ad rewarded_instance");
+                self.rewarded_instance = rewardedVideo;
+                self.rewarded_instance.loadAsync()
+                    .then( (retVal) => {
+                        console.log("[Adverts] Preloaded rewarded ad");
+                        if(waitForLoad) waitForLoad();
+                    })
+                    .catch( (err) => {
+                        switch(err){
+                            case 'ADS_FREQUENT_LOAD':
+                            case 'ADS_NO_FILL':
+                            case 'INVALID_PARAM':
+                            case 'NETWORK_FAILURE':
+                                break;
+                        }
+
+                        console.error('[Adverts] Load rewarded ad failed! ' + err.code);
+                        console.error(err.message);
+
+                        if(waitForLoad) waitForLoad();
+                    });
+                if(callback) callback();
+            })
+            .catch((err) => {
+                switch(err){
+                    case 'ADS_TOO_MANY_INSTANCES':
+                    case 'CLIENT_UNSUPPORTED_OPERATION':
+                        break;
+                }
+
+                console.error('[Adverts] Get ad rewarded_instance failed! ' + err.code);
+                console.error(err.message);
+
+                if(callback) callback();
+                if(waitForLoad) waitForLoad();
+            });
+    }
+
+    showRewardedAd(callback){
         "use strict";
         let self = this;
 
@@ -86,13 +154,12 @@ class Adverts {
             return;
         }
 
-        this.instance.showAsync()
+        this.rewarded_instance.showAsync()
             .then( () => {
-                self._loaded = false;
                 if(callback)
                     callback();
-                self.instance = null;
-                self.initialize();
+                self.rewarded_instance = null;
+                self._getAndLoadRewardedAdInstance();
             })
             .catch( (err) => {
                 // ad not watched
@@ -102,8 +169,50 @@ class Adverts {
                     case 'NETWORK_FAILURE':
                         break;
                 }
-                console.error('[Adverts] ShowAd failed! ' + err.code);
-                console.error(err.message);
+                console.error('[Adverts] showRewardedAd failed! ' + err.code);
+                console.log(err.message);
+                console.log(err);
+
+                if(callback)
+                    callback();
+            });
+    }
+
+    showInterstitialAd(callback){
+        "use strict";
+        let self = this;
+
+        if(this.enabled === false){
+            if(callback)
+                callback();
+            return;
+        }
+
+        if(this._loaded === false){
+            console.error("[Adverts] showAd failed! Ad not loaded!");
+            if(callback)
+                callback();
+            return;
+        }
+
+        this.interstitial_instance.showAsync()
+            .then( () => {
+                if(callback)
+                    callback();
+                self.interstitial_instance = null;
+                self._getAndLoadInterstitialAdInstance();
+            })
+            .catch( (err) => {
+                // ad not watched
+                switch(err){
+                    case 'ADS_NOT_LOADED':
+                    case 'INVALID_PARAM':
+                    case 'NETWORK_FAILURE':
+                        break;
+                }
+                console.error('[Adverts] showInterstitialAd failed! ' + err.code);
+                console.log(err.message);
+                console.log(err);
 
                 if(callback)
                     callback();
